ab467129b44b3fbf19153ee5bed72f07ec159369
Vahe1209 <Vahe1209@gmail.com>
2021-08-04 14:38:50 +0400

    fix: css for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,17 +8,54 @@ export default function Header({ onChange }) {
     onChange(target.value);
   };
   return (
-    <Menu style={{ padding: "20px", backgroundColor: "#333", margin: 0 }}>
-      <Row justify="space-between" align="middle">
-        <Col flex="300px" style={{ height: "100px" }}>
+    <Menu
+      style={{
+        padding: "20px",
+        backgroundColor: "hsl(0deg 0% 0% / 40%)",
+        margin: 0,
+      }}
+    >
+      <Row
+        justify="space-between"
+        align="middle"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Col
+          flex="300px"
+          style={{
+            height: "100px",
+            marginTop: "15px",
+          }}
+        >
           <Logo />
         </Col>
-        <Col flex="500px">
-          <Input placeholder="Search" onChange={onSearch} />
+        <Col flex="400px">
+          <Input
+            style={{
+              width: "100%",
+              height: "30px",
+              borderRadius: "15px",
+              padding: "5px",
+              border: "2px solid black",
+            }}
+            placeholder="Search"
+            onChange={onSearch}
+          />
         </Col>
-        <Col flex="150px">
-          <LikeOutlined className="icon" />
-          <LogoutOutlined className="icon" />
+        <Col
+          flex="300px"
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <LikeOutlined style={{ fontSize: "50px" }} />
+          <LogoutOutlined style={{ fontSize: "50px" }} />
         </Col>
       </Row>
     </Menu>
